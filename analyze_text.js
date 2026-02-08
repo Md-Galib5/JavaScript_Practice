16501925ba7cff050803bdf1946ff9cbc4401fad
Md-Galib5 <mdgalibcp05@gmail.com>
2026-02-08 17:13:16 +0600

    assignment complete

---
+++ analyze_text.js
@@ -0,0 +1,33 @@
+function analyzeText(str) {
+    // Your code here
+     if(typeof str !== "string") {
+        return "Invalid";
+    }
+    if(str.length === 0){
+        return "Invalid";
+    }
+    let words = str.split(" ");
+
+    let max = "";
+    let ch = 0;
+    let word = 0;
+
+    for(word of words){
+        if(word.length > max.length){
+            max = word;
+        }
+    }
+
+    let count = 0;
+    
+    for(ch of str){
+        if(ch !== " "){
+            count++;
+        }
+    }
+
+    return {
+        longwords: max,
+        token: count
+    };
+}
